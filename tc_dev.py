084d1378ebe69a8d8be9b9be9b904a970a7686c8
Parlin.Yu <bupt.aswmtjdsj@gmail.com>
2014-08-04 05:08:49 +0800

    little fix for importing

--- tc_dev.py
+++ tc_dev.py
@@ -1,5 +1,6 @@
-import bs4
-from bs4 import BeautifulSoup as bs
+#!/usr/bin/python
+import BeautifulSoup as bs4
+from BeautifulSoup import BeautifulSoup as bs
 import requests
 callurl = 'http://community.topcoder.com/tc?module=ActiveContests&pt=39'
 r=requests.post(callurl)
@@ -10,9 +11,11 @@ cnt = 0
 cItem=[]
 for i in a.findAll('table', {"class": "stat"}):
     for j in i.findAll('tr'):
-        if j.has_attr('class'):
+        if len(j.attrs) >0 and u'class' in j.attrs[0]:
             print "Contest #{}: ".format(cnt),
             cnt += 1
             for k in j.findAll('td'):
-                print map(lambda x: x.strip() if type(x) != bs4.element.Tag else x, k.contents), " | ",
+                print map(lambda x: x.strip() if type(x) != bs4.Tag else x.contents, k.contents), " | ",
             print ""
+        else:
+            print j.contents
